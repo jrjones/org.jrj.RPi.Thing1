28ad2f27d5bdd32fce03e5e05f3cfef2b5b417c5
Joseph R. Jones <jrj@jrj.org>
2015-06-13 21:04:27 -0700

    initial commit

---
+++ Thing1/LedBulb.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using Windows.UI;
+using Windows.Devices.Gpio;
+
+namespace org.jrj.RPi.Thing1
+{ 
+    /// <summary>
+    /// A controll class for an LED bulb connected to a GPIO pin
+    /// </summary>
+    class LedBulb
+    {
+        private GpioPin pin;
+        private GpioPinValue pinValue;
+        private GpioController gpio;
+
+        /// <summary>
+        /// Initializes the LED so it can be used. Needs to be done at application launch.
+        /// </summary>
+        public void Initialize()
+        {
+            this.gpio = GpioController.GetDefault();
+
+            //TODO - Need to handle case where GPIO not found if (gpio == null)
+
+            this.pin = gpio.OpenPin(this.PinNumber);
+            this.pinValue = GpioPinValue.Low;
+            this.pin.Write(pinValue);
+            this.pin.SetDriveMode(GpioPinDriveMode.Output);
+            this.ison = true;
+
+            return;
+        }
+
+        /// <summary>
+        /// Turns the LED on at full power
+        /// </summary>
+        public void TurnOn()
+        {
+            this.pinValue = GpioPinValue.High;
+            this.pin.Write(pinValue);
+            this.pin.SetDriveMode(GpioPinDriveMode.Output);
+            this.ison = true;
+
+            return;
+        }
+
+        /// <summary>
+        /// Turns the LED off
+        /// </summary>
+        public void TurnOff()
+        {
+            this.pinValue = GpioPinValue.Low;
+            this.pin.Write(pinValue);
+            this.pin.SetDriveMode(GpioPinDriveMode.Output);
+            this.ison = false;
+            
+            return;
+        }
+
+        /// <summary>
+        /// Toggles the LED (If on, turns off. If off, turns on.)
+        /// </summary>
+        public void Toggle()
+        {
+            if (this.isOn)
+            {
+                this.TurnOff();
+            }
+            else
+            {
+                this.TurnOn();
+            }
+            return;
+        }
+
+
+        /// <summary>
+        /// Is the bulb currently lit?
+        /// </summary>
+        public bool isOn
+        {
+            get
+            {
+                return ison;
+            }
+        }
+        private bool ison = false;
+
+        /// <summary>
+        /// The color of the bulb.
+        /// </summary>
+        public Windows.UI.Color Color
+        {
+            get
+            {
+                return this.color;
+            }
+
+            set
+            {
+                this.color = value;
+                return;
+            }
+        }
+        private Windows.UI.Color color;
+
+        /// <summary>
+        /// The pin number the bulb is connected to on the GPIO bus
+        /// </summary>
+        public int PinNumber
+        {
+            get
+            {
+                return pinNumber;
+            }
+
+            set
+            {
+                pinNumber = value;
+                return;
+            }
+        }
+        private int pinNumber;
+    }
+}
---
+++ Thing1/MainPage.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+/*  Thing1 is a quick hello world app for Raspberry Pi. Right now, it's not doing anything interesting.
+    jrj@jrj.org
+
+    (Item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409)
+*/
+
+namespace org.jrj.RPi.Thing1
+{
+    /// <summary>
+    /// This is the main form for the app, shows a couple of software "lights" and buttons linked to the coresponding LEDs
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private LedBulb RedBulb = new LedBulb();
+        private LedBulb GreenBulb = new LedBulb();
+        private LedBulb BlueBulb = new LedBulb();
+
+        // We will use these brushes to color the software lights
+        private SolidColorBrush redBrush = new SolidColorBrush(Windows.UI.Colors.Red);
+        private SolidColorBrush greenBrush = new SolidColorBrush(Windows.UI.Colors.Green);
+        private SolidColorBrush grayBrush = new SolidColorBrush(Windows.UI.Colors.Gray);
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            GreenBulb.Color = Colors.Green;
+            GreenBulb.PinNumber = 12;
+            GreenBulb.Initialize();
+            GreenBulb.TurnOn();
+
+            RedBulb.Color = Colors.Red;
+            RedBulb.PinNumber = 5;
+            RedBulb.Initialize();
+            RedBulb.TurnOn();
+
+            BlueBulb.Color = Colors.Blue;
+            BlueBulb.PinNumber = 6;
+            BlueBulb.Initialize();
+            BlueBulb.TurnOn();
+        }
+
+        private void ButtonLEDRed_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.RedBulb.isOn)
+            {
+                this.LED_R.Fill = new SolidColorBrush(RedBulb.Color);
+            }
+            else
+            {
+                this.LED_R.Fill = grayBrush;
+            }
+            this.RedBulb.Toggle();
+        }
+        
+        private void ButtonLEDGreen_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.GreenBulb.isOn)
+            {
+                this.LED_G.Fill = new SolidColorBrush(GreenBulb.Color);
+            }
+            else
+            {
+                this.LED_G.Fill = grayBrush;
+            }
+            this.GreenBulb.Toggle();
+        }
+
+        private void ButtonLEDBlue_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.BlueBulb.isOn)
+            {
+                this.LED_B.Fill = new SolidColorBrush(BlueBulb.Color);
+            }
+            else
+            {
+                this.LED_B.Fill = grayBrush;
+            }
+            this.BlueBulb.Toggle();
+        }
+    }
+}
